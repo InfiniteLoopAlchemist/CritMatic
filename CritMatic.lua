4fc723288afed4ab9d34f29cf0d0c8256adce4dc
InfiniteLøøpAlchemist <141493045+InfiniteLoopAlchemist@users.noreply.github.com>
2023-09-01 16:05:36 -0400

    Initial commit

---
+++ CritMatic.lua
@@ -0,0 +1,271 @@
+-- Define a table to hold the highest hits data.
+CritMaticData = CritMaticData or {}
+
+local MAX_HIT = 40000
+
+local function GetGCD()
+  local _, gcdDuration = GetSpellCooldown(78) -- 78 is the spell ID for Warrior's Heroic Strike
+  if gcdDuration == 0 then
+    return 1.5 -- Default GCD duration if not available (you may adjust this value if needed)
+  else
+    return gcdDuration
+  end
+end
+-- Function to create a new frame based on the template
+CritMatic.CreateNewMessageFrame = function()
+  local f = CreateFrame("Frame", nil, UIParent)
+  f:SetSize(750, 30)
+  f:SetPoint("CENTER", UIParent, "CENTER", 0, 350)
+
+  f.text = f:CreateFontString(nil, "ARTWORK", "GameFontNormalHuge")
+  f.text:SetAllPoints()
+
+  f.bounce = f:CreateAnimationGroup()
+  local scaleUp = f.bounce:CreateAnimation("Scale")
+  scaleUp:SetScale(1.5, 1.5)
+  scaleUp:SetDuration(0.15)
+  scaleUp:SetOrder(1)
+
+  local fontPath = "Interface\\AddOns\\CritMatic\\Fonts\\8bit.ttf"
+  f.text:SetFont(fontPath, 20, "THICKOUTLINE")
+  f.text:SetShadowOffset(3, -3)
+
+  return f
+end
+
+local function removeImproved(spellName)
+  -- Stripping out "Improved " prefix
+  local baseSpellName = spellName
+  if spellName and string.sub(spellName, 1, 8) == "Improved" then
+    baseSpellName = string.sub(spellName, 10)
+  end
+  return baseSpellName
+end
+
+local function IsSpellInSpellbook(spellName)
+  local name = GetSpellInfo(spellName)
+  return name ~= nil
+end
+
+local function AddHighestHitsToTooltip(self, slot, isSpellBook)
+  if (not slot) then
+    return
+  end
+  local actionType, id, spellName, castTime
+  if isSpellBook then
+    -- Handle spellbook item
+    spellName = GetSpellBookItemName(slot, BOOKTYPE_SPELL)
+    id, _, _, castTime = GetSpellInfo(spellName)
+    actionType = "spell"
+  else
+    -- Handle action bar item
+    actionType, id = GetActionInfo(slot)
+    spellName, _, _, castTime = GetSpellInfo(id)
+  end
+  if actionType == "spell" then
+
+    local baseSpellName = removeImproved(spellName)
+
+    if CritMaticData[baseSpellName] then
+
+      local cooldown = (GetSpellBaseCooldown(id) or 0) / 1000
+      --TODO: if the action is less than the GCD, use the GCD instead
+      local effectiveCastTime = castTime > 0 and (castTime / 1000) or GetGCD()
+      local effectiveTime = max(effectiveCastTime, cooldown)
+
+      local critHPS = CritMaticData[baseSpellName].highestHealCrit / effectiveTime
+      local normalHPS = CritMaticData[baseSpellName].highestHeal / effectiveTime
+      local critDPS = CritMaticData[baseSpellName].highestCrit / effectiveTime
+      local normalDPS = CritMaticData[baseSpellName].highestNormal / effectiveTime
+
+      local CritMaticHealLeft = "Highest Heal Crit: "
+      local CritMaticHealRight = tostring(CritMaticData[baseSpellName].highestHealCrit) .. " (" .. format("%.1f", critHPS) .. " HPS)"
+      local normalMaticHealLeft = "Highest Heal Normal: "
+      local normalMaticHealRight = tostring(CritMaticData[baseSpellName].highestHeal) .. " (" .. format("%.1f", normalHPS) .. " HPS)"
+
+      local CritMaticLeft = "Highest Crit: "
+      local CritMaticRight = tostring(CritMaticData[baseSpellName].highestCrit) .. " (" .. format("%.1f", critDPS) .. " DPS)"
+      local normalMaticLeft = "Highest Normal: "
+      local normalMaticRight = tostring(CritMaticData[baseSpellName].highestNormal) .. " (" .. format("%.1f", normalDPS) .. " DPS)"
+
+      -- Check if lines are already present in the tooltip.
+      local critMaticHealExists = false
+      local normalMaticHealExists = false
+      local critMaticExists = false
+      local normalMaticExists = false
+
+      for i = 1, self:NumLines() do
+        local gtl = _G["GameTooltipTextLeft" .. i]
+        local gtr = _G["GameTooltipTextRight" .. i]
+
+        if gtl and gtr then
+          -- Healing related
+          if gtl:GetText() == CritMaticHealLeft and gtr:GetText() == CritMaticHealRight then
+            critMaticHealExists = true
+          elseif gtl:GetText() == normalMaticHealLeft and gtr:GetText() == normalMaticHealRight then
+            normalMaticHealExists = true
+          end
+          -- Damage related
+          if gtl:GetText() == CritMaticLeft and gtr:GetText() == CritMaticRight then
+            critMaticExists = true
+          elseif gtl:GetText() == normalMaticLeft and gtr:GetText() == normalMaticRight then
+            normalMaticExists = true
+          end
+        end
+      end
+
+      if CritMaticData[baseSpellName].highestHeal > 0 or CritMaticData[baseSpellName].highestHealCrit > 0 then
+
+        if not critMaticHealExists then
+          self:AddDoubleLine(CritMaticHealLeft, CritMaticHealRight, 0.9, 0.9, 0.9, 0.9, 0.82, 0)
+          _G["GameTooltipTextLeft" .. self:NumLines()]:SetTextColor(1, 1, 1) -- left side color (white)
+          _G["GameTooltipTextRight" .. self:NumLines()]:SetTextColor(1, 0.82, 0) -- right side color (gold)
+        end
+
+        if not normalMaticHealExists then
+          self:AddDoubleLine(normalMaticHealLeft, normalMaticHealRight, 0.9, 0.9, 0.9, 0.9, 0.82, 0)
+          _G["GameTooltipTextLeft" .. self:NumLines()]:SetTextColor(1, 1, 1) -- left side color (white)
+          _G["GameTooltipTextRight" .. self:NumLines()]:SetTextColor(1, 0.82, 0) -- right side color (gold)
+        end
+      end
+      -- This is a damaging spell
+      if CritMaticData[baseSpellName].highestNormal > 0 or CritMaticData[baseSpellName].highestCrit > 0 then
+        if not critMaticExists then
+          self:AddDoubleLine(CritMaticLeft, CritMaticRight, 0.9, 0.9, 0.9, 0.9, 0.82, 0)
+          _G["GameTooltipTextLeft" .. self:NumLines()]:SetTextColor(1, 1, 1) -- left side color (white)
+          _G["GameTooltipTextRight" .. self:NumLines()]:SetTextColor(1, 0.82, 0) -- right side color (gold)
+        end
+        if not normalMaticExists then
+          self:AddDoubleLine(normalMaticLeft, normalMaticRight, 0.9, 0.9, 0.9, 0.9, 0.82, 0)
+          _G["GameTooltipTextLeft" .. self:NumLines()]:SetTextColor(1, 1, 1) -- left side color (white)
+          _G["GameTooltipTextRight" .. self:NumLines()]:SetTextColor(1, 0.82, 0) -- right side color (gold)
+        end
+      end
+
+      self:Show()
+    end
+  end
+end
+
+-- Register an event that fires when the player hits an enemy.
+local f = CreateFrame("FRAME")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+f:SetScript("OnEvent", function(self, event, ...)
+
+  if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+    local eventInfo = { CombatLogGetCurrentEventInfo() }
+
+    local _, eventType, _, sourceGUID, _, _, _, destGUID = unpack(eventInfo)
+    local _, spellID, spellName, spellSchool, amount, overhealing, absorbed, critical
+    if eventType == "SWING_DAMAGE" then
+      spellName = "Auto Attack"
+      spellID = 6603 -- or specify the path to a melee icon, if you have one
+      amount, _, _, _, _, _, critical = unpack(eventInfo, 12, 18)
+    elseif eventType == "SPELL_HEAL" or eventType == "SPELL_PERIODIC_HEAL" then
+      spellID, spellName, spellSchool = unpack(eventInfo, 12, 14)
+      amount, overhealing, absorbed, critical = unpack(eventInfo, 15, 18)
+    elseif eventType == "SPELL_DAMAGE" or eventType == "SPELL_PERIODIC_DAMAGE" then
+      spellID, spellName, spellSchool = unpack(eventInfo, 12, 14)
+      amount, overhealing, _, _, _, absorbed, critical = unpack(eventInfo, 15, 21)
+    end
+
+    local baseSpellName = removeImproved(spellName)
+
+    if baseSpellName == "Auto Attack" then
+      return
+    end
+
+    if sourceGUID == UnitGUID("player") or sourceGUID == UnitGUID("pet") and destGUID ~= UnitGUID("player") and (eventType == "SPELL_DAMAGE" or eventType == "SWING_DAMAGE" or eventType == "RANGE_DAMAGE" or eventType == "SPELL_HEAL" or eventType == "SPELL_PERIODIC_HEAL" or eventType == "SPELL_PERIODIC_DAMAGE") and amount > 0 then
+      if baseSpellName then
+        CritMaticData[baseSpellName] = CritMaticData[baseSpellName] or {
+          highestCrit = 0,
+          highestNormal = 0,
+          highestHealCrit = 0,
+          highestHeal = 0,
+          spellIcon = GetSpellTexture(spellID)
+        }
+        if IsSpellInSpellbook(baseSpellName) then
+          --print(CombatLogGetCurrentEventInfo())
+
+          if eventType == "SPELL_HEAL" or eventType == "SPELL_PERIODIC_HEAL" then
+            if critical then
+
+              -- When the event is a heal and it's a critical heal.
+              if amount > CritMaticData[baseSpellName].highestHealCrit and amount <= MAX_HIT then
+                CritMaticData[baseSpellName].highestHealCrit = amount
+                PlaySound(888, "SFX")
+                CritMatic.ShowNewHealCritMessage(baseSpellName, amount)
+                print("New highest crit heal for " .. baseSpellName .. ": " .. CritMaticData[baseSpellName].highestHealCrit)
+              end
+            elseif not critical then
+              if amount > CritMaticData[baseSpellName].highestHeal and amount <= MAX_HIT then
+                CritMaticData[baseSpellName].highestHeal = amount
+                PlaySoundFile("Interface\\AddOns\\CritMatic\\Sounds\\Heaven.ogg", "SFX")
+                CritMatic.ShowNewHealMessage(baseSpellName, amount)
+                print("New highest normal heal for " .. baseSpellName .. ": " .. CritMaticData[baseSpellName].highestHeal)
+              end
+            end
+          elseif eventType == "SPELL_DAMAGE" or eventType == "SWING_DAMAGE" or eventType == "SPELL_PERIODIC_DAMAGE" then
+            if critical then
+              -- When the event is damage and it's a critical hit.
+              if amount > CritMaticData[baseSpellName].highestCrit and amount <= MAX_HIT then
+                CritMaticData[baseSpellName].highestCrit = amount
+                PlaySound(888, "SFX")
+                CritMatic.ShowNewCritMessage(baseSpellName, amount)
+                print("New highest crit hit for " .. baseSpellName .. ": " .. CritMaticData[baseSpellName].highestCrit)
+              end
+            elseif not critical then
+              -- When the event is damage but it's not a critical hit.
+              if amount > CritMaticData[baseSpellName].highestNormal and amount <= MAX_HIT then
+                CritMaticData[baseSpellName].highestNormal = amount
+                PlaySoundFile("Interface\\AddOns\\CritMatic\\Sounds\\Heroism_Cast.ogg", "SFX")
+                CritMatic.ShowNewNormalMessage(baseSpellName, amount)
+                print("New highest normal hit for " .. baseSpellName .. ": " .. CritMaticData[baseSpellName].highestNormal)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end)
+Critmatic = LibStub("AceAddon-3.0"):NewAddon("|cffffd700CritMatic|r", "AceConsole-3.0","AceTimer-3.0")
+local function CritMaticLoaded()
+  Critmatic:Print("|cff918d86 v|r|cffd3cfc7 0.1.61 |r|cff918d86 Loaded!|r")
+end
+
+function Critmatic:TimerCritMaticLoaded()
+  CritMaticLoaded()
+end
+-- Called when the addon is loaded
+function Critmatic:OnInitialize()
+  -- Register console commands
+  Critmatic:RegisterChatCommand("cmreset", "CritMaticReset")
+  CritMaticData = _G["CritMaticData"]
+
+  hooksecurefunc(GameTooltip, "SetAction", AddHighestHitsToTooltip)
+  local GameTooltip = IsAddOnLoaded("ElvUI") and _G.ElvUISpellBookTooltip or _G.GameTooltip
+  hooksecurefunc(GameTooltip, "SetSpellBookItem", AddHighestHitsToTooltip)
+  local f = CritMatic.CreateNewMessageFrame()
+
+  if IsAddOnLoaded("ElvUI") then
+    self:ScheduleTimer("TimerCritMaticLoaded", 5)
+  else
+    CritMaticLoaded()
+  end
+end
+  -- Called when the addon is enabled
+function Critmatic:OnEnable()
+
+end
+
+-- Called when the addon is disabled
+function Critmatic:OnDisable()
+
+end
+
+function Critmatic:CritMaticReset()
+  CritMaticData = {}
+  Critmatic:Print("|cffff0000Data Reset!|r")
+end
---
+++ README.md
@@ -0,0 +1,103 @@
+# WOTLK Classic 3.4.2 and Classic / Hardcore
+
+## CritMatic v0.1.56-release - Your Personal Damage and Heal Tracker
+
+CritMatic is a lightweight World of Warcraft addon designed to help players keep track of their highest critical and
+normal hits (both damage and heal).
+
+### NOTE:
+
+#### Some spells require a party to track the spell. because of wow.exe game optimization
+
+#### There is a short calibration period were you will see a lot of notifications at first but it will quickly simmer down.
+
+### Key Features:
+
+- **Real-time Tracking:** CritMatic monitors your combat logs in real-time and records your highest critical and normal
+  hits for each spell you cast. It automatically updates these records when you hit harder.
+- **Healing Tracking:** In addition to damage, CritMatic also tracks your healing output, recording the highest heal and
+  critical heal for each healing spell you cast.
+
+- **Detailed Tooltip Information:** When you mouse over a spell on your action bar, CritMatic adds additional lines to
+  the tooltip showing your highest normal and critical hit with that spell.
+
+- **Display Damage and Heals notification:** CritMatic will notify you with a message and sound effect whenever you hit
+  a new highest
+  normal or critical hit.
+
+- **Persistent Data:** CritMatic saves your highest hits data between sessions, so you can log out or switch characters
+  without losing your records. You can also reset the data at any time if you want to start fresh.
+
+- **Slash Command:** Use the `/cmreset` command to reset all your CritMatic saved data. This can be useful if you want
+  to start tracking from scratch.
+
+With CritMatic you'll always know when you've hit a new personal best. Happy Criting!
+
+## Planned:
+
+- Configuration options for more fonts, colors and more.
+- Chat Messages are gold for crits.
+- Crit-log by level and ability.
+- Customizing the Crit/Normal/Heal notification message and sounds.
+- Option to disable the Crit/Normal/Heal notification message and sounds.
+- Option to disable the CritMatic tooltip information.
+- option to move the CritMatic Crit/Normal/Heal notification message.
+- GUI Interface for configuration.
+
+## Known Issues:
+
+- Warlocks Healthstones don't display in the tooltip or message.
+- pets are not currently tracked in the tooltip or message.
+- Some spells like Killing Spree / Starfall show really low dps.
+
+## Change Log:
+
+### [v0.1.6-release] - 8/29/2023
+
+#### Fixed
+
+- Fixed a bug that would not display the first Crit/Normal/Heal notification when just starting the game.
+
+### [v0.1.56-release] - 8/28/2023
+
+#### Added
+
+- Added A new Heal Sound SFX for Normal Heals.
+
+### [v0.1.55-release] - 8/27/2023
+
+Fixed A bug where there was no sound for normal hit/heals
+
+#### Added
+
+- Added Support for Classic Era / Hardcore.
+- Updated Notification Animation.
+
+### [v0.1.5-alpha] - 8/26/2023
+
+#### Added
+
+- Added Support for Classic Era / Hardcore.
+- Updated the Notification Animation.
+
+### [v0.1.4-alpha] - 8/23/2023
+
+#### Added
+
+- Added new Crit animation for crits.
+- Added CritMatic tooltip information to the spell-book.
+
+### [v0.1.3-alpha] - 8/20/2023
+
+#### Fixed
+
+- A bug where hit messages disappearing too fast.
+
+### [v0.1.2-alpha] - 8/18/2023
+
+#### Added
+
+- Added a max for crits,heals and normal hits so on some bosses you don't get a 1 million crits.
+- Added a check to prevent CritMatic from attempting to track spells that are not in your spell-book.
+
+
