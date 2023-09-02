eb2f309706c73cd16ea06e1d76a66faae4a1c703
InfiniteLøøpAlchemist <141493045+InfiniteLoopAlchemist@users.noreply.github.com>
2023-09-02 09:32:01 -0400

    Refactored Script

--- tag_and_update_version.sh
+++ tag_and_update_version.sh
@@ -6,12 +6,12 @@ git tag $1
 # Get only the latest Git tag name
 VERSION=$(git describe --tags --abbrev=0)
 
-# Update version in .toc files
-sed -i "s/^## Version: .*/## Version: $VERSION/" C:/Users/looot/OneDrive/Documents/WOW-Addons/CritMatic/CritMatic_Vanila.toc
-sed -i "s/^## Version: .*/## Version: $VERSION/" C:/Users/looot/OneDrive/Documents/WOW-Addons/CritMatic/CritMatic_Wrath.toc
+# Update version in .toc files C:/Users/looot/OneDrive/Documents/WOW-Addons/CritMatic/
+sed -i "s/^## Version: .*/## Version: $VERSION/" ./CritMatic_Vanila.toc
+sed -i "s/^## Version: .*/## Version: $VERSION/" ./CritMatic_Wrath.toc
 
 # Update version in README.md
-sed -i "s/^## CritMatic: .*/## CritMatic: $VERSION/" C:/Users/looot/OneDrive/Documents/WOW-Addons/CritMatic/README.md
+sed -i "s/^## CritMatic: .*/## CritMatic: $VERSION/" ./README.md
 
 # Push the new tag to the remote Git repository
 git push origin $VERSION
