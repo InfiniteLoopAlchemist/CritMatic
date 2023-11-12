197240b6753073c7598e8dd104116e5326d41ef2
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-11-12 11:25:43 -0500

    [v0.3.6.2.4-release] - 11/12/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,18 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.3.6.2.4-release] - 11/12/2023
+
+ Fixed:
+
+ Addressed the issue where critical hits (crits) were not consistently displayed correctly in the CritMatic addon, a bug fix has been implemented. This should resolve the intermittent inaccuracies in the alerts and chat crit display.
+
+ a bug has been fixed that was preventing new critical hits (crits) from being announced in battlegrounds, raids, and parties. This update ensures that crit announcements are correctly broadcasted in these group environments.
+
+
+
+
+
 [v0.3.6.2.3-release] - 11/11/2023
 
  Fixed:
@@ -221,14 +233,6 @@ Critmatic.showChangeLog = function()
  Updated:
 
  General improvements and bug fixes.
-
-
-
-[v0.2.5.6-release] - 9/27/2023
-
- Updated:
-
- Minor revisions and optimizations.
 
 
 ]]
