171d431cd4f15ea56a86248b0d20b6623baeb231
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-11-25 13:51:47 -0500

    [v0.3.8.1-release] - 11/25/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,20 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.3.8.1-release] - 11/25/2023
+
+ Fixed:
+
+ Good news! We've fixed a bug that affected how the crit log displayed information for spells that can both heal and damage. Previously, if a spell had both of these capabilities, the crit log wasn't displaying correctly.
+
+ This issue did not affect spells that were exclusively for healing or damage. With this latest update, you can expect accurate and consistent display for all spells, regardless of whether they heal, damage, or do both.
+
+ Thank you for your continued support and feedback, which helps us improve CritMatic. Enjoy your enhanced gaming experience!
+
+
+
+
+
 [v0.3.7.1-release] - 11/20/2023
 
  Updated:
@@ -227,15 +241,6 @@ Critmatic.showChangeLog = function()
  Added:
 
  Included toggle for silencing party notifications.
-
-
-
-[v0.3.0-release] - 10/25/2023
-
- Added:
-
- New Slash Commands to open the options menu /cm and /critmatic
- You can Change the Crit and Normal hit / heal sounds.
 
 
 ]]
