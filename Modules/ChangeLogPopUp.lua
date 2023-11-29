cf77b9071f183c2f265719b713b383fee7670ca3
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-11-29 11:44:46 -0500

    [v0.3.8.7-release] - 11/29/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,11 +35,13 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
-[v0.3.8.6-release] - 11/27/2023
+[v0.3.8.7-release] - 11/29/2023
 
- Added:
+ Updated:
 
- CritMatic was translated to traditional Chinese using Google Translate. So if it made a mistake, let me know on discord. I only speak English. Sorry for any connivance.
+ We need your help translating CritMatic to other languages just visit the localization tab on https://legacy.curseforge.com/wow/addons/critmatic to start. 
+
+ You will translation credit in the addon and on CurseForge.
 
 
 
