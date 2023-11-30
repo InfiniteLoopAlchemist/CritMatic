b9f709f948a0177717fb429e8b68077b3c8a22dd
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-11-30 18:14:04 -0500

    [v0.3.9-release] - 11/30/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,25 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.3.9-release] - 11/30/2023
+
+ Fixed:
+
+ Resolved issue with /cmcritlog slash command not functioning as expected.
+
+ Additional slash commands have been tested and confirmed operational.
+
+
+ Added:
+
+ You can now ignore specific spells from tracking by using /cmignore Spell Name. Make sure the spelling is correct, but it's case insensitive.
+
+ Use /cmhelp for even more slash commands.
+
+
+
+
+
 [v0.3.8.8-release] - 11/29/2023
 
  Updated:
