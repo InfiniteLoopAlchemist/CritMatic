bd8cff4769f52f82967ae0a0c5cbb7cc7d781cfa
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-11-30 18:37:57 -0500

    [v0.3.9.1-release] - 11/30/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,16 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.3.9.1-release] - 11/30/2023
+
+ Fixed:
+
+ Various Fixes Implemented
+
+
+
+
+
 [v0.3.9-release] - 11/30/2023
 
  Fixed:
