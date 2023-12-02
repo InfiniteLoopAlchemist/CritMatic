5f2a90fce48f423f5094e606a0621c0a9fcf3912
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-12-02 18:36:50 -0500

    [v0.4.0-release] - 12/2/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,21 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.4.0-release] - 12/2/2023
+
+ Fixed:
+
+ Fixed issue where Crit Log shifts right on Reload/Logout.
+
+
+ Added:
+
+ To reset specific spell data, use the command /cmdeletespelldata spell name This avoids the need to clear all data with /cmreset.
+
+
+
+
+
 [v0.3.9.8-release] - 12/1/2023
 
  Added:
