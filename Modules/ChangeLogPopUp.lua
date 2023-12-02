39461d5c715bdaa2278ba21817ff05bdf23bfb1a
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-12-02 13:44:45 -0500

    [v0.3.9.8-release] - 12/1/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,16 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.3.9.8-release] - 12/1/2023
+
+ Added:
+
+ Added a new slash command to reset the position of Crit Log if it goes off screen. Use /cmcritlogdefaultpos Causes a ReloadUI.
+
+
+
+
+
 [v0.3.9.7-release] - 12/1/2023
 
 
