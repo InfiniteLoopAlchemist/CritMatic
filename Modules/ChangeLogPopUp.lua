64c7712bebd0e6e22bfcebb4fa64e67d27f793a7
InfiniteLoopAlchemist <InfiniteLoopAlchemit@pm.me>
2023-12-05 06:15:11 -0500

    [v0.4.2.1-release] - 12/4/2023

--- Modules/ChangeLogPopUp.lua
+++ Modules/ChangeLogPopUp.lua
@@ -35,6 +35,21 @@ Critmatic.showChangeLog = function()
     -- Add the change log text
     local changelog = [[
 
+[v0.4.2.1-release] - 12/4/2023
+
+ Updated:
+
+ Removed ignore Auto Attack from options. You can use /cmignore spell name to ignore Auto Attack instead.
+
+
+ Added:
+
+ You can now customize the alert notification message. Use /cm Its under Alert. Soon chat messages too.
+
+
+
+
+
 [v0.4.1-release] - 12/3/2023
 
  Updated:
